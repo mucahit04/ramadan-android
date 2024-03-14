7b068f6f2b6023fc788b4b215221f2ec0efbb16d
Mucahit Kahraman <57506115+mucahit04@users.noreply.github.com>
2024-03-14 08:58:48 +0000

    files copied

---
+++ src/app/events-list.ts
@@ -0,0 +1,92 @@
+export const dayList = [
+    {   count: 1,
+        event: '3 Euro harclik'
+    },
+    {   count: 2,
+        event: 'Surprizli top'
+    },
+    {   count: 3,
+        event: 'UNO gecesi'
+    },
+    {   count: 4,
+        event: '2 Euro harclik'
+    },
+    {   count: 5,
+        event: 'Kopek ve evi'
+    },
+    {   count: 6,
+        event: 'Ailecek Nintendo oynama'
+    },
+    {   count: 7,
+        event: 'Anne kiz bakim gunu'
+    },
+    {   count: 8,
+        event: 'Cuzdan'
+    },
+    {   count: 9,
+        event: 'Aile etkinlik saati(sen sec)'
+    },
+    {   count: 10,
+        event: '3 Euro harclik'
+    },
+    {   count: 11,
+        event: 'Dondurma'
+    },
+    {   count: 12,
+        event: 'Kupe'
+    },
+    {   count: 13,
+        event: 'Ailecek kutu oyunu'
+    },
+    {   count: 14,
+        event: '2 Euro harclik'
+    },
+    {   count: 15,
+        event: 'Oyuncak'
+    },
+    {   count: 16,
+        event: 'Taki yapma'
+    },
+    {   count: 17,
+        event: 'UNO gecesi'
+    },
+    {   count: 18,
+        event: '3 Euro harclik'
+    },
+    {   count: 19,
+        event: 'Dondurma'
+    },
+    {   count: 20,
+        event: '2 Euro harclik'
+    },
+    {   count: 21,
+        event: 'Anne kiz bakim saati'
+    },
+    {   count: 22,
+        event: 'Oyuncak'
+    },
+    {   count: 23,
+        event: '2 Euro harclik'
+    },
+    {   count: 24,
+        event: 'Bir etkinlik(sen sec)'
+    },
+    {   count: 25,
+        event: 'Ailecek kutu oyunu'
+    },
+    {   count: 26,
+        event: '3 Euro harclik'
+    },
+    {   count: 27,
+        event: 'Arabada iftar'
+    },
+    {   count: 28,
+        event: 'Dondurma'
+    },
+    {   count: 29,
+        event: 'Kiyafet'
+    },
+    {   count: 30,
+        event: '3 Euro harclik'
+    },
+];
